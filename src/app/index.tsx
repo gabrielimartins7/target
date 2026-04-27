4c55244b826a3aba47f67ff1b433e73cd542bdd8
martins.gabrieli.07.almeida@outlook.com <martins.gabrieli.07.almeida@outlook.com>
2026-04-27 10:54:13 -0300

    Expo router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native";
+import { HomeHeader } from "@/components/HomeHeader";
+import { View } from "react-native";
+
 
 export default function Index(){
     return (
-        <View style={{ flex: 1, justifyContent: "center" }}>
-            <Text>Olá, Expo Router</Text>
+        <View style={{ flex: 1 }}>
+            <HomeHeader />
         </View>
     )
 }
